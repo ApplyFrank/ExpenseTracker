1fb0ac415eaac8e48ea31b827fd4087fa1ab6aac
Frank Su <frank.su@applydigital.co>
2024-06-25 23:20:06 -0700

    Add modal and ways to differentiate navigation title

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -1,7 +1,51 @@
-import { Text } from "react-native";
+import { useLayoutEffect } from "react";
+import { View, StyleSheet } from "react-native";
+import { GlobalStyles } from "../constants/styles";
+import IconButton from "../components/UI/IconButton";
 
-function ManageExpense() {
-  return <Text>Recent Expenses</Text>;
+function ManageExpense({ route, navigation }) {
+  const editedExpenseId = route.params?.expenseId;
+  const isEditing = !!editedExpenseId;
+
+  // FRANK NOTE best pratice to wrap navigation.setOptions in a useLayoutEffect
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: isEditing ? "Edit Expense" : "Add Expense",
+    });
+  }, [navigation, isEditing]);
+
+  function deleteExpenseHandler() {}
+
+  console.log(editedExpenseId);
+  return (
+    <View style={styles.container}>
+      {isEditing && (
+        <View style={styles.deleteContainer}>
+          <IconButton
+            icon="trash"
+            color={GlobalStyles.colors.error500}
+            size={36}
+            onPress={deleteExpenseHandler}
+          />
+        </View>
+      )}
+    </View>
+  );
 }
 
 export default ManageExpense;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: GlobalStyles.colors.primary800,
+  },
+  deleteContainer: {
+    marginTop: 16,
+    paddingTop: 8,
+    borderTopWidth: 2,
+    borderTopColor: GlobalStyles.colors.primary200,
+    alignItems: "center",
+  },
+});
